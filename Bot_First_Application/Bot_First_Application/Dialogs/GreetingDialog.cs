39d94157f01958d73dc6ba1b5e7e39047ec05e0a
anowakowski <nowakowski.add@gmail.com>
2017-11-27 22:25:41 +0100

    Added new file for HotelBotDialog.cs and RoomReservation.cs with functionality

--- Bot_First_Application/Bot_First_Application/Dialogs/GreetingDialog.cs
+++ Bot_First_Application/Bot_First_Application/Dialogs/GreetingDialog.cs
@@ -11,8 +11,25 @@ namespace Bot_First_Application.Dialogs
     {
         public async Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync("Hi Im a robot");
+            await context.PostAsync("Hi I'm a robot");
+            await Respond(context);
+
             context.Wait(MessageRecivedAsync);
+        }
+
+        private static async Task Respond(IDialogContext context)
+        {
+            var userName = string.Empty;
+            context.UserData.TryGetValue<string>("Name", out userName);
+            if (string.IsNullOrEmpty(userName))
+            {
+                await context.PostAsync("What is your name?");
+                context.UserData.SetValue<bool>("GetName", true);
+            }
+            else
+            {
+                await context.PostAsync(string.Format("Hi {0}. How can I help you today?", userName));
+            }
         }
 
         private async Task MessageRecivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
@@ -20,6 +37,7 @@ namespace Bot_First_Application.Dialogs
             var message = await result;
             var userName = string.Empty;
             var getName = false;
+
             context.UserData.TryGetValue<string>("Name", out userName);
             context.UserData.TryGetValue<bool>("GetName", out getName);
 
@@ -30,17 +48,8 @@ namespace Bot_First_Application.Dialogs
                 context.UserData.SetValue<bool>("GetName", false);
             }
 
-            if (string.IsNullOrEmpty(userName))
-            {
-                await context.PostAsync("What is your name?");
-                context.UserData.SetValue<bool>("GetName", true);
-            }
-            else
-            {
-                await context.PostAsync(string.Format("Hi {0}. How can I help you today?", userName));
-            }
-
-            context.Wait(MessageRecivedAsync);
+            await Respond(context);
+            context.Done(message);
         }
     }
 }
---
+++ Bot_First_Application/Bot_First_Application/Models/RoomReservation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_First_Application.Models
+{
+    public enum BedSizeOptions
+    {
+        King,
+        Queen,
+        Single,
+        Double
+    }
+
+    public enum AmenituesOptions
+    {
+        Kitchen,
+        ExtraTowels,
+        GymAccess,
+        Wifi
+    }
+
+    [Serializable]
+    public class RoomReservation
+    {
+        public BedSizeOptions? BedSize;
+        public int? NumberOfOccupants;
+        public DateTime? CheckInDate;
+        public int? NumberOfDayToStay;
+        public List<AmenituesOptions> Amenities;
+
+        public static IForm<RoomReservation> BuildForm()
+        {
+            return new FormBuilder<RoomReservation>()
+                .Message("Welcome to the hotel reservation")
+                .Build();
+        }
+    }
+}
